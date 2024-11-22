96a799998d2c4818b66eefed22cce8acf1abc05c
Aguila1989 <k.demeulenaere@hotmail.com>
2024-11-22 16:58:36 +0100

    added 2 new blog posts

--- src/components/subPages/blog/Blog.vue
+++ src/components/subPages/blog/Blog.vue
@@ -13,6 +13,25 @@ export default {
 <template>
   <h1>Blog</h1>
   <section class="blog">
+    <BlogCard title="Tech and Meet AI: The reality behind the hype" date="2024-11-21"
+              :paragraphs="[
+              {description: 'On November 21, 2024, I attended a fascinating presentation by ir. Wim Delvaux. The event was scheduled to start at 6:30 PM but, due to a misunderstanding, it only began at 8:00 PM. Despite the delay, the insights shared made it well worth the wait.'},
+              {description: 'The presentation explored the essence of intelligence and how AI attempts to replicate human cognitive processes like observing, learning, reasoning, problem-solving, and language use. Delvaux elaborated on two primary approaches to AI: bottom-up (connectionist), which mimics biological systems using neural networks, and top-down (symbolic), which relies on logical structures like knowledge graphs and databases.'},
+              {description: 'He also highlighted the societal impact of AI, emphasizing its transformative potential alongside critical risks, such as overhyped promises and ethical concerns. Notably, he discussed the importance of EU regulations to ensure transparency, mitigate risks, and safeguard public interests in this rapidly evolving domain.'},
+              {description: 'This event served as a reminder that while AI offers incredible possibilities, it demands careful scrutiny and responsible implementation to avoid pitfalls.'},
+              ]"
+              sourceImageRight="src/assets/images/ai de realiteit achter de hype.jpg" imageRightAlt="TuxTalk" />
+
+    <BlogCard title="Hackathon Hack the Future" date="2022-11-19"
+              :paragraphs="[
+              {description: 'On November 19, 2024, I had the incredible opportunity to participate in the Hackathon Hack the Future, an event designed to push the boundaries of creativity and technical skills. <br>Together with my teammate Clint, we embarked on a thrilling journey to develop a program for astronaut selection using the Solid framework.'},
+              {description: 'The project aimed to streamline the astronaut selection process by creating dynamic challenges, maintaining a real-time leaderboard, and incorporating a robust search feature. Although we didn’t take home a prize, the experience was both rewarding and deeply enriching.'},
+              {description: 'Our day began with an introduction to the hackathon’s theme, which encouraged us to think outside the box while solving real-world problems. After brainstorming ideas, we decided to focus on the astronaut selection program, given its unique challenges and scope for innovation. <br>The Solid framework proved to be an exciting tool, offering a decentralized approach to managing data and fostering privacy, a key consideration in scenarios like astronaut training.<br> Throughout the day, Clint and I collaborated closely, tackling obstacles and refining our application to make it as efficient and user-friendly as possible.'},
+              {description: 'The event itself was exceptionally well-organized, reflecting the thoughtful planning of the organizers. Delicious meals and refreshing drinks kept our energy high, while generous goodie bags added a touch of excitement to the day. <br>Every detail seemed tailored to ensure participants had everything they needed to succeed, fostering an atmosphere of motivation and camaraderie. <br>During breaks, we took the opportunity to explore a nearby zoo, which provided an interesting diversion. However, our enjoyment was tempered by the small enclosures for the animals, which left us feeling a mix of fascination and sadness.'},
+              {description: 'Despite not winning, the hackathon offered invaluable lessons and unforgettable moments. We honed our technical skills, learned to think critically under pressure, and deepened our understanding of innovative frameworks like Solid. <br>Most importantly, the event reinforced the joy of creating something meaningful while collaborating with like-minded individuals. <br>The inspiring talks and interactions with other participants further enriched the experience, sparking ideas and new perspectives.'}
+              ]"
+              sourceImageLeft="src/assets/images/hack the future.jpg"
+              imageLeftAlt="baserow" />
 
     <BlogCard title="Tech and Meet TuxTalk" date="2024-10-29"
               :paragraphs="[
